6f8d27ac46d20beb9ab47d4e4f352f7e600bd87a
confehtti <69188627+confehtti@users.noreply.github.com>
2021-05-19 01:42:57 -0400

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,136 @@
+// -------------Hamburger----------------------------
+
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
+
+function toggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
+
+
+//------------------------Modal for Projects ---------------------------//
+
+//------Graphic Design Modals-------------
+
+// project 1
+const open_graphic_one = document.getElementById('open_graphic_one')
+const modal_graphic_container_one = document.getElementById('modal_graphic_container_one')
+const close_graphic_one = document.getElementById('close_graphic_one')
+
+open_graphic_one.addEventListener('click', () => {
+    modal_graphic_container_one.classList.add('show')
+})
+
+close_graphic_one.addEventListener('click', () => {
+    modal_graphic_container_one.classList.remove('show')
+})
+
+//project 2
+const open_graphic_two = document.getElementById('open_graphic_two')
+const modal_graphic_container_two = document.getElementById('modal_graphic_container_two')
+const close_graphic_two = document.getElementById('close_graphic_two')
+
+open_graphic_two.addEventListener('click', () => {
+    modal_graphic_container_two.classList.add('show')
+})
+
+close_graphic_two.addEventListener('click', () => {
+    modal_graphic_container_two.classList.remove('show')
+})
+
+//project 3
+const open_graphic_three = document.getElementById('open_graphic_three')
+const modal_graphic_container_three = document.getElementById('modal_graphic_container_three')
+const close_graphic_three = document.getElementById('close_graphic_three')
+
+open_graphic_three.addEventListener('click', () => {
+    modal_graphic_container_three.classList.add('show')
+})
+
+close_graphic_three.addEventListener('click', () => {
+    modal_graphic_container_three.classList.remove('show')
+})
+
+//project 4
+const open_graphic_four = document.getElementById('open_graphic_four')
+const modal_graphic_container_four = document.getElementById('modal_graphic_container_four')
+const close_graphic_four = document.getElementById('close_graphic_four')
+
+open_graphic_four.addEventListener('click', () => {
+    modal_graphic_container_four.classList.add('show')
+})
+
+close_graphic_four.addEventListener('click', () => {
+    modal_graphic_container_four.classList.remove('show')
+})
+
+//project 5
+const open_graphic_five = document.getElementById('open_graphic_five')
+const modal_graphic_container_five = document.getElementById('modal_graphic_container_five')
+const close_graphic_five = document.getElementById('close_graphic_five')
+
+open_graphic_five.addEventListener('click', () => {
+    modal_graphic_container_five.classList.add('show')
+})
+
+close_graphic_five.addEventListener('click', () => {
+    modal_graphic_container_five.classList.remove('show')
+})
+
+//project 6
+const open_graphic_six = document.getElementById('open_graphic_six')
+const modal_graphic_container_six = document.getElementById('modal_graphic_container_six')
+const close_graphic_six = document.getElementById('close_graphic_six')
+
+open_graphic_six.addEventListener('click', () => {
+    modal_graphic_container_six.classList.add('show')
+})
+
+close_graphic_six.addEventListener('click', () => {
+    modal_graphic_container_six.classList.remove('show')
+})
+
+//project 7
+const open_graphic_seven = document.getElementById('open_graphic_seven')
+const modal_graphic_container_seven = document.getElementById('modal_graphic_container_seven')
+const close_graphic_seven = document.getElementById('close_graphic_seven')
+
+open_graphic_seven.addEventListener('click', () => {
+    modal_graphic_container_seven.classList.add('show')
+})
+
+close_graphic_seven.addEventListener('click', () => {
+    modal_graphic_container_seven.classList.remove('show')
+})
+
+
+//-------Fine Art & Digital Modals--------------
+
+// project 1
+const open_art_one = document.getElementById('open_art_one')
+const modal_art_container_one = document.getElementById('modal_art_container_one')
+const close_art_one = document.getElementById('close_art_one')
+
+open_art_one.addEventListener('click', () => {
+    modal_art_container_one.classList.add('show')
+})
+
+close_art_one.addEventListener('click', () => {
+    modal_art_container_one.classList.remove('show')
+})
+
+// project 2
+const open_art_two = document.getElementById('open_art_two')
+const modal_art_container_two = document.getElementById('modal_art_container_two')
+const close_art_two = document.getElementById('close_art_two')
+
+open_art_two.addEventListener('click', () => {
+    modal_art_container_two.classList.add('show')
+})
+
+close_art_two.addEventListener('click', () => {
+    modal_art_container_two.classList.remove('show')
+})
+
